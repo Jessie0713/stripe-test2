19d1694d4ce1f14fe03070fcf0ee28bc04e207d7
許湘潔 <>
2022-03-21 17:12:56 +0800

    test

---
+++ .cache/_this_is_virtual_fs_path_/$virtual/async-requires.js
@@ -0,0 +1,6 @@
+exports.components = {
+  "component---cache-dev-404-page-js": () => import("./../../dev-404-page.js" /* webpackChunkName: "component---cache-dev-404-page-js" */),
+  "component---src-pages-aaa-js": () => import("./../../../src/pages/aaa.js" /* webpackChunkName: "component---src-pages-aaa-js" */),
+  "component---src-pages-index-js": () => import("./../../../src/pages/index.js" /* webpackChunkName: "component---src-pages-index-js" */)
+}
+
